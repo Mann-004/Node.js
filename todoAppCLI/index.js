05ee3d87f8829de622bc0ab0ac8a8568d73b6df1
Mann <randhawamanpreet37@gmail.com>
2025-07-13 13:16:58 +0530

    final commit

--- todoAppCLI/index.js
+++ todoAppCLI/index.js
@@ -2,7 +2,43 @@ import readline from "readline"
 import { stdin, stdout } from "process"
 
 const rl = readline.createInterface({
-    input:stdin,
-    output:stdout
+    input: stdin,
+    output: stdout
 })
 
+const todos = []
+
+const show = () => {
+    console.log("\n1:Create a task")
+    console.log("2:View all task")
+    console.log("3:Exit")
+
+    rl.question('Choose one option:', handleInput)
+}
+
+const handleInput = (option) => {
+    if(option === "1"){
+        rl.question("Enter a task :",(task) => {
+            todos.push(task)
+            console.log("created task :", task)
+            show()
+      })
+    }
+    else if( option === "2"){
+        console.log("\nAll tasks are :")
+        todos.forEach((task, index) => {
+            console.log(`${index + 1} : ${task}`)
+        })
+        show()
+        
+    }else if(option === "3"){
+        console.log("Goode bye")
+        rl.close()
+
+    }else{
+        console.log("Invalid option,PLease try again")
+        show()
+    }
+
+}
+show()
